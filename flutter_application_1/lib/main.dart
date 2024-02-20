ceb0f5c0df807a16d238604cd854b1c47b5fc60c
Hala Albahloul <hala.albahloul@gmail.com>
2024-02-20 11:15:19 +0100

    Update UI

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -9,70 +9,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Function App',
+      title: 'NoteAI',
       home: HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _audioUploaded = false;
+  bool _slidesUploaded = false;
+
+  void _navigateToFunctionsScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => FunctionsScreen()),
+    );
+  }
+
+  void _navigateToSumAndNotesScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SumAndNotesScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('App Functions'),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FunctionButton(
-              buttonText: 'Summarize',
-              onPressed: () {
-                // Logic for Summarize button
-              },
-            ),
-            FunctionButton(
-              buttonText: 'Notes',
-              onPressed: () {
-                // Logic for Notes button
-              },
-            ),
-            FunctionButton(
-              buttonText: 'Align with slides',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => AlignWithSlidesScreen()),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class AlignWithSlidesScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Align with Slides'),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
       ),
       body: Center(
         child: Column(
@@ -84,13 +54,10 @@ class AlignWithSlidesScreen extends StatelessWidget {
                 FilePickerResult? result = await FilePicker.platform.pickFiles(
                   type: FileType.audio,
                 );
-
                 if (result != null) {
-                  PlatformFile file = result.files.first;
-                  print(file.name);
-                  // Now you can use the file.name and file.bytes or file.path to upload or use the file
-                } else {
-                  // User canceled the picker
+                  setState(() {
+                    _audioUploaded = true;
+                  });
                 }
               },
             ),
@@ -101,18 +68,159 @@ class AlignWithSlidesScreen extends StatelessWidget {
                   type: FileType.custom,
                   allowedExtensions: ['pdf', 'ppt', 'pptx'],
                 );
-
                 if (result != null) {
-                  PlatformFile file = result.files.first;
-                  print(file.name);
-                  // Similarly, use the file as needed for upload or processing
-                } else {
-                  // User canceled the picker
+                  setState(() {
+                    _slidesUploaded = true;
+                  });
                 }
+              },
+            ),
+            if (_audioUploaded || _slidesUploaded)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_audioUploaded && _slidesUploaded) {
+                      _navigateToFunctionsScreen();
+                    } else {
+                      _navigateToSumAndNotesScreen();
+                    }
+                  },
+                  child: Text('Done'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    onPrimary: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FunctionsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select Function'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FunctionButton(
+              buttonText: 'Notes',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NotesScreen()),
+                );
+              },
+            ),
+            FunctionButton(
+              buttonText: 'Sum',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => SummarizationScreen()),
+                );
+              },
+            ),
+            FunctionButton(
+              buttonText: 'Align with Slides',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AlignmentScreen()),
+                );
               },
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SumAndNotesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sum and Notes'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FunctionButton(
+              buttonText: 'Sum',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => SummarizationScreen()),
+                );
+              },
+            ),
+            FunctionButton(
+              buttonText: 'Notes',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NotesScreen()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SummarizationScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Summarization'),
+      ),
+      body: Center(
+        child: Text('Here you can find the sum'),
+      ),
+    );
+  }
+}
+
+class NotesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+      ),
+      body: Center(
+        child: Text('Here you can find the notes'),
+      ),
+    );
+  }
+}
+
+class AlignmentScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Alignment Example'),
+      ),
+      body: Center(
+        child: Text('Here the alignments'),
       ),
     );
   }
@@ -132,8 +240,8 @@ class FunctionButton extends StatelessWidget {
         onPressed: onPressed,
         child: Text(buttonText),
         style: ElevatedButton.styleFrom(
-          primary: Colors.grey[300], // Button color
-          onPrimary: Colors.black, // Text color
+          primary: Color.fromARGB(255, 84, 98, 250), // Button color
+          onPrimary: const Color.fromARGB(255, 252, 252, 252), // Text color
           padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
         ),
       ),
